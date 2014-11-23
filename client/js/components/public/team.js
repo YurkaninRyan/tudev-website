0813bbc276e205ff1e05920dc077ef00736dddde
Ryan Yurkanin <yurkaninryan@hotmail.com>
2014-11-23 13:40:55 -0500

    garbage

--- client/js/components/public/team.js
+++ client/js/components/public/team.js
@@ -4,7 +4,28 @@ var React   = require('react');
 var Team = React.createClass({
     render: function() {
         return (
-            <div></div>
+            <div id='team-container'>
+                <div id='team-block1' className={'team-block'}>
+                    <li id='member1block' className={'memberblock'}>
+                    </li>
+                    <li id='member2block' className={'memberblock'}>
+                    </li>
+                    <li id='member3block' className={'memberblock'}>
+                    </li>
+                    <li id='member4block' className={'memberblock'}>
+                    </li>
+                </div>
+                <div id='team-block2' className={'team-block'}>
+                    <li id='member5block' className={'memberblock'}>
+                    </li>
+                    <li id='member6block' className={'memberblock'}>
+                    </li>
+                    <li id='member7block' className={'memberblock'}>
+                    </li>
+                    <li id='member8block' className={'memberblock'}>
+                    </li>
+                </div>
+            </div>
         );
     }
 });
